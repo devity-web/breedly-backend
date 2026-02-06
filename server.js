cb9066093dadbf344609c6670ace25d5d457a50a
Matheus Pedroni <pedroni.dev@gmail.com>
2026-02-06 09:37:13 +0000

    feat: test new version

---
+++ server.js
@@ -0,0 +1,28 @@
+import {ValidationPipe} from '@nestjs/common';
+import {NestFactory} from '@nestjs/core';
+import {ExpressAdapter} from '@nestjs/platform-express';
+import express from 'express';
+import {AppModule} from './dist/app.module';
+
+const server = express();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  app.useGlobalPipes(new ValidationPipe({transform: true}));
+  app.setGlobalPrefix('api');
+  app.enableCors();
+
+  await app.init();
+
+  return server;
+}
+
+export default async function handler(req, res) {
+  try {
+    const serverInstance = await bootstrap();
+    serverInstance(req, res);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+}
