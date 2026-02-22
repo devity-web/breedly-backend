023387d8456677968611d6d5fcc7f9fd7795019d
Matheus Pedroni <pedroni.dev@gmail.com>
2026-02-22 20:18:51 +0000

    feat: add dynamic imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import {Module} from '@nestjs/common';
 import {ConfigModule, ConfigService} from '@nestjs/config';
 import {PrismaModule} from '@utils/prisma.module';
 import {PrismaService} from '@utils/prisma.service';
-import {betterAuth} from 'better-auth';
-import {prismaAdapter} from 'better-auth/adapters/prisma';
 import {AuthModule} from './app/auth/auth.module';
 import {CustomerModule} from './app/customer/customer.module';
 import {DashboardModule} from './app/dashboard/dashboard.module';
@@ -15,10 +13,12 @@ import {DogModule} from './app/dog/dog.module';
     AuthModule.forRootAsync({
       imports: [PrismaModule, ConfigModule],
       inject: [PrismaService, ConfigService],
-      useFactory: (
+      useFactory: async (
         prismaService: PrismaService,
         configService: ConfigService,
       ) => {
+        const {betterAuth} = await import('better-auth');
+        const {prismaAdapter} = await import('better-auth/adapters/prisma');
         return {
           auth: betterAuth({
             database: prismaAdapter(prismaService, {
